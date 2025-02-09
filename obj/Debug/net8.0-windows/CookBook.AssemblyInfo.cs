dd64b3f2f93fa9718d3af4dae4bb9c54e6765c04
Bea-Brown <bea.leonardi@hotmail.com>
2025-02-09 13:02:08 -0700

    5

--- obj/Debug/net8.0-windows/CookBook.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/CookBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc5368fcac305ee54e383a68c90c39c08b7f3013")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92a484de82e825714bbe5f4e4aa2a8f7b9f03604")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
